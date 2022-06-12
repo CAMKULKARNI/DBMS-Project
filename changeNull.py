9502940533f8d4006cadc229958aba10ba1746b7
Chinmay Kulkarni <camkoolkarni@gmail.com>
2022-06-12 11:16:51 +0530

    Data generation files

--- changeNull.py
+++
@@ -1,6 +0,0 @@
-with open("new.txt", "r") as f:
-    with open("new1.txt", "a") as f1:
-        j = 0
-        for i in f.readlines():
-            f1.write(i.replace("NULL", f"'{j:04}'"))
-            j += 1
--- gen.py
+++
@@ -1,5 +0,0 @@
-import random
-with open("gen.txt", "w") as f:
-    for i in range(364):
-        f.write(
-            f"INSERT into PARTICIPATES_IN values ('{i*2:04}', {random.randint(1, 41)})\n")
